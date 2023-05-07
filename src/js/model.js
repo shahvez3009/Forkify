a2dc37571cf91338f023f8e5a8737634bf3e265e
Shahvez Mahmood <Shahvez01@gmail.com>
2023-05-07 18:14:35 +0200

    Refactoring

---
+++ src/js/model.js
@@ -0,0 +1,27 @@
+import { async } from "regenerator-runtime";
+import { API_URL } from "./config.js";
+import { getJson } from "./helpers.js";
+
+export const state = {
+  recipe: {},
+};
+
+export const loadRecipe = async (id) => {
+  try {
+    const data = await getJson(`${API_URL}/${id}`);
+    const { recipe } = data.data;
+    state.recipe = {
+      id: recipe.id,
+      title: recipe.title,
+      publisher: recipe.publisher,
+      sourceUrl: recipe.source_url,
+      image: recipe.image_url,
+      servings: recipe.servings,
+      cookingTime: recipe.cooking_time,
+      ingredients: recipe.ingredients,
+    };
+    console.log(state.recipe);
+  } catch (err) {
+    console.error(`${err}🤨`);
+  }
+};
